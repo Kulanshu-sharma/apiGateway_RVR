6c4c7808f124038479457da5d862a70c55e7d20e
Kulanshu Sharma <kulanshusharma@192.168.1.14>
2022-08-03 17:10:50 +0530

    Creating API Gateway RVR Project

---
+++ src/main/java/com/RVR/apiGateway/apiGateway_RVR/ApiGatewayRvrApplication.java
@@ -0,0 +1,13 @@
+package com.RVR.apiGateway.apiGateway_RVR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayRvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayRvrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/RVR/apiGateway/apiGateway_RVR/ApiGatewayRvrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RVR.apiGateway.apiGateway_RVR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayRvrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
